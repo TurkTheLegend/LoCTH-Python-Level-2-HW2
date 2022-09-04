73406983577e9a317b81d0f81e555ca6e987d06d
0xTurk <97168241+TurkTheLegend@users.noreply.github.com>
2022-09-04 14:02:19 +0700

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,14 +1,11 @@
-import string
-
-
 def TelNumberCheck(x):
     if x[0] != "+" :
-        raise LookupError("Error Code must start with +")
+        raise LookupError("The phone number must start with +")
     elif not ("0" <= x[1] <= "9" and "0" <= x[2] <= "9"):
-        raise ValueError("Error Code must be + and followed by 2 digits")
+        raise ValueError("The phone number must be + and followed by 2 digits")
     elif len(x) != 13 :
        raise IndexError("The phone number does not match the length specified") #เวเขียนโจทย์ผิด
-    elif type(x) != string :
+    elif type(x) != str :
         raise TypeError("The phone number must be a String")
     if x[:3] == "+66":
         return "Thailand"
--- 4.py
+++ 4.py
@@ -1,7 +1,7 @@
 def TelNumberCheck(x):
-    assert x[0] == "+" ,'Error Code must start with "+"'
-    assert "0" <= x[1] <= "9" and "0" <= x[2] <= "9" ,"Error Code must be + and followed by 2 digits"
-    assert len(x) == 13 , "The phone number does not match the length specified" #เวเขียนโจทย์ผิด
+    assert x[0] == "+" , 'The phone number must start with "+"'
+    assert "0" <= x[1] <= "9" and "0" <= x[2] <= "9" ,"The Phone Number must be + and followed by 2 digits"
+    assert len(x) == 13 , "The phone number does not match the length specified" 
     assert type(x) == str ,"The phone number must be a String"
     if x[:3] == "+66":
         return "Thailand"
